b4b93c803f35dee8563c6c9eee647a5d8ca184f3
Yibing-S <76006606+Yibing-S@users.noreply.github.com>
2021-04-27 02:13:07 -0400

    add first sql query

---
+++ sql_scripts/data_pull_SQL.sql
@@ -0,0 +1,313 @@
+# 1
+select * from application limit 100;
+select * from bureau limit 100;
+select * from bureau_balance limit 100;
+select * from previous_application limit 100;
+# 2&3
+describe application;
+describe bureau;
+describe bureau_balance;
+describe previous_application;
+# 4
+select count(*), count(distinct sk_id_curr) 
+from application; 
+select count(*), count(distinct sk_id_curr) 
+from bureau; 
+select count(*), count(distinct SK_ID_BUREAU) 
+from bureau_balance; 
+
+select count(*), count(sk_id_curr), count(distinct sk_id_curr) 
+from previous_application; 
+
+select * from bureau limit 100;
+# 5
+select credit_type, count(*), count(distinct sk_id_curr)
+from bureau group by 1;
+-- one customer can help multiple same credit products
+
+# 6
+select a.*,
+b.SK_BUREAU_ID,
+b.CREDIT_ACTIVE,
+b.CREDIT_CURRENCY,
+b.DAYS_CREDIT,
+b.AMT_CREDIT_SUM,
+b.AMT_CREDIT_SUM_DEBT,
+b.CREDIT_TYPE
+from application as a
+left join bureau as b
+on a.sk_id_curr=b.sk_id_curr
+where a.sk_id_curr=215354;
+
+# 7 
+-- select count(distinct amt_credit) from application;
+select AMT_CREDIT, 
+count(*) from application 
+group by 1 limit 6000;
+
+# 8
+select target, count(*) from application 
+group by 1;
+
+# 9
+select NAME_CONTRACT_TYPE, 
+count(*) from application group by 1;
+
+# 10
+
+select *,
+AMT_CREDIT/AMT_GOODS_PRICE as NEW_CREDIT_TO_GOODS_RATIO,
+OWN_CAR_AGE/DAYS_BIRTH as NEW_CAR_TO_BIRTH_RATIO,
+AMT_CREDIT/AMT_INCOME_TOTAL as NEW_CREDIT_TO_INCOME_RATIO-- one of the most important variable! DTI
+from application;
+
+# 11
+
+-- select * from bureau where sk_id_curr=215354;
+select * from bureau where sk_id_curr=215354;
+
+select distinct credit_active from bureau;
+
+select CREDIT_ACTIVE, 
+credit_type, 
+count(distinct SK_ID_CURR) from bureau 
+group by 1,2;
+-- select * from bureau_balance where sk_id_bureau=5714463;
+#select * from bureau_balance where sk_id_bureau=5714463;
+describe bureau;
+
+# 12
+select SK_ID_CURR, CREDIT_ACTIVE,
+max(DAYS_CREDIT),
+min(DAYS_CREDIT),
+avg(DAYS_CREDIT),
+max(CREDIT_DAY_OVERDUE),
+avg(CREDIT_DAY_OVERDUE)
+from bureau 
+-- use one account to test: 
+-- where sk_id_curr=215354
+group by 1,2;
+
+# 13 flatten the data
+-- select distinct CREDIT_ACTIVE from bureau;
+
+select SK_ID_CURR,
+max(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE ='Bad Debt' then DAYS_CREDIT else null end) as bd_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT else null end) as bd_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT else null end) as bd_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE='Closed' then CREDIT_DAY_OVERDUE else null end) as cl_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE='Active' then CREDIT_DAY_OVERDUE else null end) as ac_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE='Sold' then CREDIT_DAY_OVERDUE else null end) as sd_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE ='Bad Debt' then CREDIT_DAY_OVERDUE else null end) as bd_max_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Closed' then CREDIT_DAY_OVERDUE else null end) as cl_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Active' then CREDIT_DAY_OVERDUE else null end) as ac_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Sold' then CREDIT_DAY_OVERDUE else null end) as sd_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Bad Debt' then CREDIT_DAY_OVERDUE else null end) as bd_avg_CREDIT_DAY_OVERDUE
+from bureau
+-- use one account to test: where sk_id_curr=215354
+group by 1;
+
+select count(*) from bureau;
+
+# 14
+-- describe bureau;
+/* select sk_bureau_id, credit_active, count(distinct sk_id_curr), count(*) from bureau group by 1,2
+having count(distinct sk_id_curr) <> count(*); */
+
+select SK_ID_CURR, 
+max(case when  CREDIT_ACTIVE='Bad Debt'  then 1 else 0 end) as bd_flag, 
+sum(case when  CREDIT_ACTIVE='Bad Debt'  then 1 else 0 end) as bd_num
+from bureau
+group by 1;
+
+
+# 15
+
+select * from bureau_balance;
+
+
+select distinct status from bureau_balance;
+
+# 16
+select SK_ID_BUREAU,
+avg(case when status = 'C' then 1 else 0 end) as status_c_mean,
+avg(case when status = 'X' then 1 else 0 end) as status_x_mean,
+avg(case when status = '0' then 1 else 0 end) as status_0_mean,
+avg(case when status = '1' then 1 else 0 end) as status_1_mean,
+avg(case when status = '2' then 1 else 0 end) as status_2_mean,
+avg(case when status = '3' then 1 else 0 end) as status_3_mean,
+avg(case when status = '4' then 1 else 0 end) as status_4_mean,
+avg(case when status = '5' then 1 else 0 end) as status_5_mean
+from bureau_balance
+group by 1;
+
+# 17
+select a.SK_ID_CURR,
+avg(case when status = 'C' then 1 else 0 end) as status_c_mean,
+avg(case when status = 'X' then 1 else 0 end) as status_x_mean,
+avg(case when status = '0' then 1 else 0 end) as status_0_mean,
+avg(case when status = '1' then 1 else 0 end) as status_1_mean,
+avg(case when status = '2' then 1 else 0 end) as status_2_mean,
+avg(case when status = '3' then 1 else 0 end) as status_3_mean,
+avg(case when status = '4' then 1 else 0 end) as status_4_mean,
+avg(case when status = '5' then 1 else 0 end) as status_5_mean
+from application as a
+join bureau as b
+on a.SK_ID_CURR=b.SK_ID_CURR
+join bureau_balance as c
+on b.SK_BUREAU_id=c.sk_id_bureau
+group by 1;
+
+select * from previous_application;
+
+# 18
+select count(*), count(distinct SK_ID_CURR ), count(distinct SK_ID_PREV) from previous_application;
+
+
+# 19
+select *, AMT_CREDIT/AMT_APPLICATION  as APP_CREDIT_PERC from previous_application;
+
+# 20
+select distinct NAME_CONTRACT_STATUS  from previous_application;
+
+
+select SK_ID_CURR,
+sum(case when NAME_CONTRACT_STATUS in ('Approved','Unused offer') then 1 else 0 end) as num_of_app,
+sum(case when NAME_CONTRACT_STATUS in ('Refused') then 1 else 0 end) as num_of_ref
+from previous_application group by 1;
+
+# 21
+
+select SK_ID_CURR,
+sum(case when NAME_CONTRACT_STATUS in ('Approved','Unused offer') 
+then 1 else 0 end) as num_of_app,
+sum(case when NAME_CONTRACT_STATUS in ('Refused') 
+then 1 else 0 end) as num_of_ref,
+avg(case when NAME_CONTRACT_STATUS in ('Approved') 
+then AMT_CREDIT / AMT_APPLICATION else null/*why use null?*/ end) as avg_APP_CREDIT_PERC
+from previous_application group by 1;
+
+
+# 22 final one
+select
+base.*,
+base2.status_c_mean,
+base2.status_x_mean,
+base2.status_0_mean,
+base2.status_1_mean,
+base2.status_2_mean,
+base2.status_3_mean,
+base2.status_4_mean,
+base2.status_5_mean,
+base3.num_of_app,
+base3.num_of_ref,
+base3.avg_APP_CREDIT_PERC
+from
+(select a.*, 
+AMT_CREDIT/AMT_ANNUITY as NEW_CREDIT_TO_ANNUITY_RATIO,
+AMT_CREDIT/AMT_GOODS_PRICE as NEW_CREDIT_TO_GOODS_RATIO,
+OWN_CAR_AGE/DAYS_BIRTH as NEW_CAR_TO_BIRTH_RATIO,
+OWN_CAR_AGE/DAYS_EMPLOYED as NEW_CAR_TO_EMPLOY_RATIO,
+AMT_CREDIT/AMT_INCOME_TOTAL as NEW_CREDIT_TO_INCOME_RATIO, -- one of the most important variable! DTI
+AMT_ANNUITY/AMT_INCOME_TOTAL as NEW_ANNUITY_TO_INCOME_RATIO,
+c.cl_max_DAYS_CREDIT,
+c.cl_min_DAYS_CREDIT,
+c.cl_avg_DAYS_CREDIT,
+c.ac_max_DAYS_CREDIT,
+c.ac_min_DAYS_CREDIT,
+c.ac_avg_DAYS_CREDIT,
+c.sd_max_DAYS_CREDIT,
+c.sd_min_DAYS_CREDIT,
+c.sd_avg_DAYS_CREDIT,
+c.bd_max_DAYS_CREDIT,
+c.bd_min_DAYS_CREDIT,
+c.bd_avg_DAYS_CREDIT,
+c.cl_max_CREDIT_DAY_OVERDUE,
+c.ac_max_CREDIT_DAY_OVERDUE,
+c.sd_max_CREDIT_DAY_OVERDUE,
+c.bd_max_CREDIT_DAY_OVERDUE,
+c.cl_avg_CREDIT_DAY_OVERDUE,
+c.ac_avg_CREDIT_DAY_OVERDUE,
+c.sd_avg_CREDIT_DAY_OVERDUE,
+c.bd_avg_CREDIT_DAY_OVERDUE,
+c.bd_flag,
+c.bd_num
+from
+application as a
+left join 
+(
+select SK_ID_CURR,
+max(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT else null end) as cl_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT else null end) as ac_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT else null end) as sd_avg_DAYS_CREDIT,
+max(case when CREDIT_ACTIVE ='Bad Debt' then DAYS_CREDIT else null end) as bd_max_DAYS_CREDIT,
+min(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT else null end) as bd_min_DAYS_CREDIT,
+avg(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT else null end) as bd_avg_DAYS_CREDIT,
+
+max(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT_ENDDATE else null end) as cl_max_DAYS_CREDIT_ENDDATE,
+min(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT_ENDDATE else null end) as cl_min_DAYS_CREDIT_ENDDATE,
+avg(case when CREDIT_ACTIVE='Closed' then DAYS_CREDIT_ENDDATE else null end) as cl_avg_DAYS_CREDIT_ENDDATE,
+max(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT_ENDDATE else null end) as ac_max_DAYS_CREDIT_ENDDATE,
+min(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT_ENDDATE else null end) as ac_min_DAYS_CREDIT_ENDDATE,
+avg(case when CREDIT_ACTIVE='Active' then DAYS_CREDIT_ENDDATE else null end) as ac_avg_DAYS_CREDIT_ENDDATE,
+max(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT_ENDDATE else null end) as sd_max_DAYS_CREDIT_ENDDATE,
+min(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT_ENDDATE else null end) as sd_min_DAYS_CREDIT_ENDDATE,
+avg(case when CREDIT_ACTIVE='Sold' then DAYS_CREDIT_ENDDATE else null end) as sd_avg_DAYS_CREDIT_ENDDATE,
+max(case when CREDIT_ACTIVE ='Bad Debt' then DAYS_CREDIT_ENDDATE else null end) as bd_max_DAYS_CREDIT_ENDDATE,
+min(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT_ENDDATE else null end) as bd_min_DAYS_CREDIT_ENDDATE,
+avg(case when CREDIT_ACTIVE='Bad Debt' then DAYS_CREDIT_ENDDATE else null end) as bd_avg_DAYS_CREDIT_ENDDATE,
+
+max(case when CREDIT_ACTIVE='Closed' then CREDIT_DAY_OVERDUE else null end) as cl_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE='Active' then CREDIT_DAY_OVERDUE else null end) as ac_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE='Sold' then CREDIT_DAY_OVERDUE else null end) as sd_max_CREDIT_DAY_OVERDUE,
+max(case when CREDIT_ACTIVE ='Bad Debt' then CREDIT_DAY_OVERDUE else null end) as bd_max_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Closed' then CREDIT_DAY_OVERDUE else null end) as cl_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Active' then CREDIT_DAY_OVERDUE else null end) as ac_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Sold' then CREDIT_DAY_OVERDUE else null end) as sd_avg_CREDIT_DAY_OVERDUE,
+avg(case when CREDIT_ACTIVE='Bad Debt' then CREDIT_DAY_OVERDUE else null end) as bd_avg_CREDIT_DAY_OVERDUE,
+max(case when  CREDIT_ACTIVE='Bad Debt'  then 1 else 0 end) as bd_flag, 
+sum(case when  CREDIT_ACTIVE='Bad Debt'  then 1 else 0 end) as bd_num
+from bureau
+group by 1) as c
+on a.SK_ID_CURR=c.SK_ID_CURR) as base
+left join
+(select a.SK_ID_CURR,
+avg(case when status = 'C' then 1 else 0 end) as status_c_mean,
+avg(case when status = 'X' then 1 else 0 end) as status_x_mean,
+avg(case when status = '0' then 1 else 0 end) as status_0_mean,
+avg(case when status = '1' then 1 else 0 end) as status_1_mean,
+avg(case when status = '2' then 1 else 0 end) as status_2_mean,
+avg(case when status = '3' then 1 else 0 end) as status_3_mean,
+avg(case when status = '4' then 1 else 0 end) as status_4_mean,
+avg(case when status = '5' then 1 else 0 end) as status_5_mean
+from application as a
+join bureau as b
+on a.SK_ID_CURR=b.SK_ID_CURR
+join bureau_balance as c
+on b.SK_BUREAU_id=c.sk_id_bureau
+group by 1) as base2 
+on base.SK_ID_CURR=base2.SK_ID_CURR
+left join
+(select SK_ID_CURR,
+sum(case when NAME_CONTRACT_STATUS in ('Approved','Unused offer') then 1 else 0 end) as num_of_app,
+sum(case when NAME_CONTRACT_STATUS in ('Refused') then 1 else 0 end) as num_of_ref,
+avg(case when NAME_CONTRACT_STATUS in ('Approved') then AMT_APPLICATION / AMT_CREDIT else null/*why use null?*/ end) as avg_APP_CREDIT_PERC
+from previous_application group by 1) base3
+on base.SK_ID_CURR=base3.SK_ID_CURR;
+
+
